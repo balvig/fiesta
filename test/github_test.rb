ba167caaf84856ef7da9c81b5b2ec12fada58db6
Jens Balvig <jens@balvig.com>
2019-04-08 17:09:21 +0900

    test actual value

--- test/github_test.rb
+++ test/github_test.rb
@@ -9,7 +9,8 @@ module Capistrano::Fiesta
     end
 
     def test_access_token_defaulting_to_env_value
-      refute Github.client.access_token.nil?, "Expected `access_token` to get set from OCTOKIT_ACCESS_TOKEN"
+      refute Github.client.access_token.nil?
+      assert_equal ENV["OCTOKIT_ACCESS_TOKEN"], Github.client.access_token, "Expected `access_token` to get set from OCTOKIT_ACCESS_TOKEN"
     end
   end
 end
