4771db8774a5b2270a0c690084d03d3371c8af07
Satoshi Matsumoto <kaorimatz@gmail.com>
2019-06-22 13:05:55 +0100

    Add unit test for Capistrano::Fiesta::Slack

---
+++ test/slack_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+module Capistrano::Fiesta
+  class SlackTest < Minitest::Test
+    def test_post
+      webhook = "https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX"
+      payload = { text: "• New login\n" }
+      stub = stub_request(:post, webhook).with(body: payload)
+      Slack.new(webhook: webhook, payload: payload).post
+      assert_requested stub
+    end
+  end
+end
